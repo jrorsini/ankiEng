1208ddf443e52f10834a4c013c1521dc21f93a15
Jean-Roger Orsini <jean-rogerorsini@jean-rogers-mbp-1.home>
2023-05-01 12:10:34 +0200

    test

--- app.js
+++ app.js
@@ -1,48 +1,62 @@
-import inquirer from 'inquirer';
-import scrapeWord from './scrape.js';
-import chalk from 'chalk';
+import inquirer from "inquirer";
+import scrapeWord from "./scrape.js";
+import chalk from "chalk";
 
 async function main() {
-	while (true) {
-		let word;
-		if (process.argv[2] !== undefined) {
-			word = process.argv[2].toLowerCase();
-		} else {
-			const answers = await inquirer.prompt([
-				{
-					type: 'input',
-					name: 'word',
-					message: 'Enter a word to search (or press Enter to quit):',
-				},
-			]);
-			word = answers.word.trim();
-		}
+    while (true) {
+        let userInput;
+        let userInputtype;
 
-		if (!word) {
-			break;
-		}
+        // user input
 
-		console.log(chalk.magenta.bold(`Searching for "${word}"...`));
+        if (process.argv.length <= 2) {
+            console.log("No user input provided.");
+        } else {
+            const userInput = process.argv.slice(2).join(" ");
+            console.log(`User input: ${userInput}`);
+        }
 
-		try {
-			const { ipa, spelling, type, definition } = await scrapeWord(word);
-			console.log(``);
-			console.log(chalk.yellow.underline.bold('Word:') + ` ${word}`);
-			console.log(chalk.yellow.underline.bold('IPA:') + ` ${ipa}`);
-			console.log(chalk.yellow.underline.bold('Spelling:') + ` ${spelling}`);
-			console.log(chalk.yellow.underline.bold('Type:') + ` ${type}`);
-			console.log(
-				chalk.yellow.underline.bold('Definition:') + ` ${definition}`
-			);
-			console.log(``);
-			console.log(`${word};${ipa};${spelling};${type};${definition}`);
-			console.log('---');
-		} catch (err) {
-			console.log(``);
-			console.log(chalk.red.bold(`could not find ${word}`));
-			console.log('---');
-		}
-	}
+        if (process.argv[2] !== undefined) {
+            userInput = process.argv[2].toLowerCase();
+        } else {
+            const answers = await inquirer.prompt([
+                {
+                    type: "input",
+                    name: "word",
+                    message: "Enter a word to search (or press Enter to quit):",
+                },
+            ]);
+            userInput = answers.word.trim();
+        }
+
+        if (!word) {
+            break;
+        }
+
+        console.log(chalk.magenta.bold(`Searching for "${word}"...`));
+
+        // scrape
+        try {
+            const { ipa, spelling, type, definition } = await scrapeWord(word);
+            console.log(``);
+            console.log(chalk.yellow.underline.bold("Word:") + ` ${word}`);
+            console.log(chalk.yellow.underline.bold("IPA:") + ` ${ipa}`);
+            console.log(
+                chalk.yellow.underline.bold("Spelling:") + ` ${spelling}`
+            );
+            console.log(chalk.yellow.underline.bold("Type:") + ` ${type}`);
+            console.log(
+                chalk.yellow.underline.bold("Definition:") + ` ${definition}`
+            );
+            console.log(``);
+            console.log(`${word};${ipa};${spelling};${type};${definition}`);
+            console.log("---");
+        } catch (err) {
+            console.log(``);
+            console.log(chalk.red.bold(`could not find ${word}`));
+            console.log("---");
+        }
+    }
 }
 
 main();
--- scrape.js
+++ scrape.js
@@ -1,136 +1,136 @@
-import axios from 'axios';
-import cheerio from 'cheerio';
-import inquirer from 'inquirer';
+import axios from "axios";
+import cheerio from "cheerio";
+import inquirer from "inquirer";
 
 async function scrapeWord(word) {
-	// website's urls
-	const dictionary_url = `https://www.dictionary.com/browse/${word}`;
-	const thesaurus_url = `https://www.thesaurus.com/browse/${word}`;
-	// body response
-	const dictionary_response = await axios.get(dictionary_url);
-	const thesaurus_response = await axios.get(thesaurus_url);
+  // website's urls
+  const dictionary_url = `https://www.dictionary.com/browse/${word}`;
+  const thesaurus_url = `https://www.thesaurus.com/browse/${word}`;
+  // body response
+  const dictionary_response = await axios.get(dictionary_url);
+  const thesaurus_response = await axios.get(thesaurus_url);
 
-	// Dictionary.com
-	let $ = cheerio.load(dictionary_response.data);
-	const ipaContainer = $('.pron-spell-ipa-container .pron-ipa-container');
-	const spellingContainer = $(
-		'.pron-spell-ipa-container .pron-spell-container'
-	);
+  // Dictionary.com
+  let $ = cheerio.load(dictionary_response.data);
+  const ipaContainer = $(".pron-spell-ipa-container .pron-ipa-container");
+  const spellingContainer = $(
+    ".pron-spell-ipa-container .pron-spell-container"
+  );
 
-	// dictionary.com ipa 
-	let ipa = '';
-	if (ipaContainer.length > 0) {
-		const ipaContents = ipaContainer.find('.pron-ipa-content');
-		if (ipaContents.length === 1) {
-			ipa = ipaContents
-				.text()
-				.trim()
-				.replace(/[/()[\]]/g, '');
-		} else {
-			const ipaChoices = ipaContents
-				.map((i, el) =>
-					$(el)
-						.text()
-						.replace(/[/()[\]]/g, '')
-						.trim()
-				)
-				.toArray();
-			let uniqueIpaChoices = [...new Set(ipaChoices)];
+  // dictionary.com ipa
+  let ipa = "";
+  if (ipaContainer.length > 0) {
+    const ipaContents = ipaContainer.find(".pron-ipa-content");
+    if (ipaContents.length === 1) {
+      ipa = ipaContents
+        .text()
+        .trim()
+        .replace(/[/()[\]]/g, "");
+    } else {
+      const ipaChoices = ipaContents
+        .map((i, el) =>
+          $(el)
+            .text()
+            .replace(/[/()[\]]/g, "")
+            .trim()
+        )
+        .toArray();
+      let uniqueIpaChoices = [...new Set(ipaChoices)];
 
-			if(uniqueIpaChoices.length > 1) {
-				const answers = await inquirer.prompt([
-					{
-						type: 'list',
-						name: 'ipaChoice',
-						message: `Multiple pronunciations found. Choose one to use for "${word}":`,
-						choices: uniqueIpaChoices,
-					},
-				]);
-				ipa = answers.ipaChoice.trim();
-			} else {
-				ipa = uniqueIpaChoices[0];
-			}
-		}
-	}
+      if (uniqueIpaChoices.length > 1) {
+        const answers = await inquirer.prompt([
+          {
+            type: "list",
+            name: "ipaChoice",
+            message: `Multiple pronunciations found. Choose one to use for "${word}":`,
+            choices: uniqueIpaChoices,
+          },
+        ]);
+        ipa = answers.ipaChoice.trim();
+      } else {
+        ipa = uniqueIpaChoices[0];
+      }
+    }
+  }
 
-	// dictionary.com spelling 
-	let spelling = '';
-	if (spellingContainer.length > 0) {
-		const spellingContents = spellingContainer.find('.pron-spell-content');
-		if (spellingContents.length === 1) {
-			spelling = spellingContents.text().trim().replace(/\[|\]/g, '');
-		} else {
-			const spellingChoices = spellingContents
-				.map((i, el) => $(el).text().replace(/\[|\]/g, '').trim())
-				.toArray();
-			let uniqueSpellingChoices = [...new Set(spellingChoices)];
+  // dictionary.com spelling
+  let spelling = "";
+  if (spellingContainer.length > 0) {
+    const spellingContents = spellingContainer.find(".pron-spell-content");
+    if (spellingContents.length === 1) {
+      spelling = spellingContents.text().trim().replace(/\[|\]/g, "");
+    } else {
+      const spellingChoices = spellingContents
+        .map((i, el) => $(el).text().replace(/\[|\]/g, "").trim())
+        .toArray();
+      let uniqueSpellingChoices = [...new Set(spellingChoices)];
 
-			if(uniqueSpellingChoices.length > 1) {
-				const answers = await inquirer.prompt([
-					{
-						type: 'list',
-						name: 'spellingChoice',
-						message: `Multiple spellings found. Choose one to use for "${word}":`,
-						choices: uniqueSpellingChoices,
-					},
-				]);
-				spelling = answers.spellingChoice.trim();
-			} else {
-				spelling = uniqueSpellingChoices[0];
-			}
-		}
-	}
-	spelling = spelling.replace(/\s+/g, ' ').trim();
+      if (uniqueSpellingChoices.length > 1) {
+        const answers = await inquirer.prompt([
+          {
+            type: "list",
+            name: "spellingChoice",
+            message: `Multiple spellings found. Choose one to use for "${word}":`,
+            choices: uniqueSpellingChoices,
+          },
+        ]);
+        spelling = answers.spellingChoice.trim();
+      } else {
+        spelling = uniqueSpellingChoices[0];
+      }
+    }
+  }
+  spelling = spelling.replace(/\s+/g, " ").trim();
 
-	// Thesaurus.com
-	$ = cheerio.load(thesaurus_response.data);
+  // Thesaurus.com
+  $ = cheerio.load(thesaurus_response.data);
 
-	const definitionChoices = [];
+  const definitionChoices = [];
 
-	$('.postab-container ul li a em')
-		.toArray()
-		.map((el, i) => {
-			definitionChoices[i] = $(el)
-				.text()
-				.trim()
-				.replace('as in', 'noun')
-				.replace('.', '');
-		});
+  $(".postab-container ul li a em")
+    .toArray()
+    .map((el, i) => {
+      definitionChoices[i] = $(el)
+        .text()
+        .trim()
+        .replace("as in", "noun")
+        .replace(".", "");
+    });
 
-	$('.postab-container ul li a strong')
-		.toArray()
-		.map((el, i) => {
-			definitionChoices[i] += ' | ' + $(el).text().trim();
-		});
+  $(".postab-container ul li a strong")
+    .toArray()
+    .map((el, i) => {
+      definitionChoices[i] += " | " + $(el).text().trim();
+    });
 
-	let tab = '';
-	let type = '';
-	let definition = '';
+  let tab = "";
+  let type = "";
+  let definition = "";
 
-	if (definitionChoices.length > 1) {
-		const answers = await inquirer.prompt([
-			{
-				type: 'list',
-				name: 'spellingChoice',
-				message: `Multiple definitions found. Choose one to use for "${word}":`,
-				choices: definitionChoices,
-			},
-		]);
-		tab = answers.spellingChoice.trim();
+  if (definitionChoices.length > 1) {
+    const answers = await inquirer.prompt([
+      {
+        type: "list",
+        name: "spellingChoice",
+        message: `Multiple definitions found. Choose one to use for "${word}":`,
+        choices: definitionChoices,
+      },
+    ]);
+    tab = answers.spellingChoice.trim();
 
-		type = answers.spellingChoice;
-		definition = answers.spellingChoice;
-	} else {
-		type = definitionChoices[0];
-		definition = definitionChoices[0];
-	}
-	type = type.trim().split(' | ')[0].replace('as in', 'noun').replace('.', '');
-	definition = definition
-		.trim()
-		.split(' | ')[1]
-		.replaceAll(';', ' -')
-		.replaceAll(',', ' -');
-	return { ipa, spelling, type, definition };
+    type = answers.spellingChoice;
+    definition = answers.spellingChoice;
+  } else {
+    type = definitionChoices[0];
+    definition = definitionChoices[0];
+  }
+  type = type.trim().split(" | ")[0].replace("as in", "noun").replace(".", "");
+  definition = definition
+    .trim()
+    .split(" | ")[1]
+    .replaceAll(";", " -")
+    .replaceAll(",", " -");
+  return { ipa, spelling, type, definition };
 }
 
 export default scrapeWord;
