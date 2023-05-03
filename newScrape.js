bfc3523535cb91c173f17a150e398018b32ecc43
Jean-Roger Orsini <jeanroger.orsini@gmail.com>
2023-05-03 13:36:13 +0200

    made some changes and added documentation above functions.

--- newScrape.js
+++ newScrape.js
@@ -1,4 +1,5 @@
 import {
+    fetchDictionaryBodyResponse,
     fetchTranslations,
     fetchExamples,
     fetchIPAs,
@@ -9,12 +10,19 @@ import {
 let type, ipa, spellings, definition, translations, examples;
 
 async function scrape(userInput) {
+    const dictionaryRes = await fetchDictionaryBodyResponse(userInput);
+
+    type = fetchTypes(userInput, dictionaryRes);
+    ipa = fetchIPAs(dictionaryRes);
+    spellings = fetchSpellings(dictionaryRes);
     translations = await fetchTranslations(userInput);
     examples = await fetchExamples(userInput);
-    ipa = await fetchIPAs(userInput);
-    spellings = await fetchSpellings(userInput);
-    type = await fetchTypes("reedy");
+
     console.log(type);
+    console.log(ipa);
+    console.log(spellings);
+    console.log(translations);
+    console.log(examples);
 }
 
 scrape("clear");
--- scrapeReverso.js
+++ scrapeReverso.js
@@ -4,6 +4,11 @@ import cheerio from "cheerio";
 import isPhrasalVerb from "./isPhrasalVerb.js";
 const reverso = new Reverso();
 
+/**
+ * Get translations from Reverso API
+ * @param {String} word - response from Reverso
+ * @returns {Array}  Translations
+ */
 export async function fetchTranslations(word) {
     let res = await reverso.getTranslation(
         word,
@@ -17,9 +22,14 @@ export async function fetchTranslations(word) {
     return res.translations;
 }
 
+/**
+ * Get examples from Reverso API
+ * @param {String} word - word to search in Reverso API
+ * @returns {Array}  Examples
+ */
 export async function fetchExamples(word) {
     let res = await reverso.getContext(
-        "dismiss",
+        word,
         "english",
         "french",
         (err, response) => {
@@ -32,14 +42,28 @@ export async function fetchExamples(word) {
     return res.examples;
 }
 
-export async function fetchIPAs(word) {
+/**
+ * retrieve body response from dictionary.com
+ * @param {String} word - word to search on dictionary.com
+ * @returns {String} html body response scraped from dicitonary.com
+ */
+export async function fetchDictionaryBodyResponse(word) {
     let body;
     try {
         body = await axios.get(`https://www.dictionary.com/browse/${word}`);
     } catch (err) {
         console.log(err, err.stack);
     }
+    return body;
+}
 
+/**
+ * List IPAs
+ * @param {String} body - dictionary.com html body from fetchDictionaryBodyResponse.
+ * @returns {Array} List of IPAs
+ */
+export function fetchIPAs(body) {
+    // arg: dictionary.com html body from fetchDictionaryBodyResponse.
     let $ = cheerio.load(body.data);
     const ipaContainer = $(".pron-spell-ipa-container").find(
         ".pron-ipa-content"
@@ -66,14 +90,12 @@ export async function fetchIPAs(word) {
           ];
 }
 
-export async function fetchSpellings(word) {
-    let body;
-    try {
-        body = await axios.get(`https://www.dictionary.com/browse/${word}`);
-    } catch (err) {
-        console.log(err, err.stack);
-    }
-
+/**
+ * List Spelling
+ * @param {String} body - dictionary.com html body from fetchDictionaryBodyResponse.
+ * @returns {Array} List of Spelling
+ */
+export function fetchSpellings(body) {
     let $ = cheerio.load(body.data);
     const spellingContainer = $(
         ".pron-spell-ipa-container .pron-spell-container"
@@ -89,14 +111,14 @@ export async function fetchSpellings(word) {
               ),
           ];
 }
-export async function fetchTypes(word) {
-    // body response
-    let body;
-    try {
-        body = await axios.get(`https://www.dictionary.com/browse/${word}`);
-    } catch (err) {
-        console.log(err, err.stack);
-    }
+
+/**
+ * List Types
+ * @param {String} word - word searched
+ * @param {String} body - dictionary.com html body from fetchDictionaryBodyResponse.
+ * @returns {Array} List of Types
+ */
+export function fetchTypes(word, body) {
     let $ = cheerio.load(body.data);
 
     return isPhrasalVerb(word)
