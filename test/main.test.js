b9f207385f2d2ea90b099b5565c45f8b03917f13
Jean-Roger Orsini <jeanroger.orsini@gmail.com>
2023-05-05 02:24:59 +0200

    changed save in file function's place and also renamed test file.

---
+++ test/main.test.js
@@ -0,0 +1,31 @@
+import isPhrasalVerb from "../utility/isPhrasalVerb.js";
+import {
+    fetchReversoResponse,
+    errorLogMessage,
+} from "../utility/scrapeFuncs.js";
+import assert from "assert";
+
+describe("Utility", function () {
+    describe("#isPhrasalVerb()", function () {
+        it("should be a phrasal verb", function () {
+            assert.equal(isPhrasalVerb("dig up"), true);
+            assert.equal(isPhrasalVerb("figure out"), true);
+        });
+        it("should should not be a phrasal verb", function () {
+            assert.equal(isPhrasalVerb("dig"), false);
+            assert.equal(isPhrasalVerb("test  "), false);
+        });
+    });
+    describe("#fetchReversoResponse()", function () {
+        it("should return a response", async function () {
+            const result = await fetchReversoResponse("test");
+            assert.equal(result.ok, true);
+        });
+        it("should return an error", async function () {
+            const result = await fetchReversoResponse("asdfadss");
+            assert.equal(result, errorLogMessage);
+        });
+    });
+});
+
+// there's a case like "multivariate" in which an error is returned from thesaurus.com cause it can't find the word.
