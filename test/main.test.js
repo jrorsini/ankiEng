825ecfd12c01329b167e053246ceec61aa9d81b1
Jean-Roger Orsini <jeanroger.orsini@gmail.com>
2023-05-06 21:37:51 +0200

    added some more test for the example fetching function.

--- test/main.test.js
+++ test/main.test.js
@@ -3,7 +3,13 @@ import {
     fetchReversoResponse,
     errorLogMessage,
     fetchTranslations,
+    fetchExamples,
 } from "../utility/scrapeFuncs.js";
+import {
+    saveWordCardToFile,
+    fileExists,
+    getFileContent,
+} from "../utility/saveWordCardToFile.js";
 import assert from "assert";
 
 describe("Utility", function () {
@@ -17,6 +23,7 @@ describe("Utility", function () {
             assert.equal(isPhrasalVerb("test  "), false);
         });
     });
+
     describe("#fetchReversoResponse()", function () {
         it("should return a response", async function () {
             const result = await fetchReversoResponse("test");
@@ -27,14 +34,36 @@ describe("Utility", function () {
             assert.equal(result, errorLogMessage);
         });
     });
+
     describe("#fetchTranslations()", function () {
         it("should return the translations without duplicates", async function () {
-            const reversoRes = await fetchReversoResponse("reedy");
-            const result = fetchTranslations(reversoRes);
-            assert.equal(result.length, 5);
+            assert.equal(
+                fetchTranslations(await fetchReversoResponse("reedy")).length,
+                5
+            );
+        });
+    });
+
+    describe("#fileExists()", function () {
+        it("should return true because the file exists", async function () {
+            assert.equal(await fileExists(), true);
+        });
+    });
+
+    describe("#getFileContent()", function () {
+        it("should return the whole content of the file", async function () {
+            const data = await getFileContent();
+            assert.equal(data.length > 0, true);
+        });
+    });
+
+    describe("#fetchExamples()", function () {
+        it("should return examples", async function () {
+            const result = await fetchReversoResponse("test");
+            const examples = fetchExamples(result);
+            assert.equal(examples.length, 3);
         });
     });
 });
 
 // there's a case like "multivariate" in which an error is returned from thesaurus.com cause it can't find the word.
-// in case of "reedy" translations there are duplicates.
