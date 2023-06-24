4674269131c03ae25b1ada7718545d7ff925f51c
Jean-Roger Orsini <jeanroger.orsini@gmail.com>
2023-06-24 17:10:24 +0200

    commented some functions that were not useful in my case.

--- anki.js
+++ anki.js
@@ -5,45 +5,37 @@ import axios from "axios";
 // Define the AnkiConnect API URL
 const ankiUrl = "http://localhost:8765/";
 
-export async function checkIfDeckExists(deckName) {
-    let deckList;
-    await axios
-        .post(ankiUrl, {
-            action: "deckNamesAndIds",
-            version: 6,
-        })
-        .then((res) => {
-            deckList = res.data.result;
-        });
-    return deckList.hasOwnProperty(deckName);
-}
+// export async function checkIfDeckExists(deckName) {
+//     let deckList;
+//     await axios
+//         .post(ankiUrl, {
+//             action: "deckNamesAndIds",
+//             version: 6,
+//         })
+//         .then((res) => {
+//             deckList = res.data.result;
+//         });
+//     return deckList.hasOwnProperty(deckName);
+// }
 
-export async function createAnkiDeck(deckName) {
-    await axios
-        .post(ankiUrl, {
-            action: "createDeck",
-            version: 6,
-            params: {
-                deck: deckName,
-            },
-        })
-        .then((res) => {
-            console.log("deck " + res.data.result + "has been created");
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-}
+// export async function createAnkiDeck(deckName) {
+//     await axios
+//         .post(ankiUrl, {
+//             action: "createDeck",
+//             version: 6,
+//             params: {
+//                 deck: deckName,
+//             },
+//         })
+//         .then((res) => {
+//             console.log("deck " + res.data.result + "has been created");
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         });
+// }
 
-export async function addCard(
-    word,
-    type,
-    pronunciation,
-    ipa,
-    definition,
-    example,
-    translation
-) {
+export async function addCard() {
     let note = {
         deckName: "ankiEng",
         modelName: "ANKIENG_NOTE",
@@ -52,29 +44,16 @@ export async function addCard(
             allowDuplicate: false,
         },
     };
-
-    const res = await axios.post(ankiUrl, {
-        action: "addNote",
-        version: 6,
-        params: {
-            note,
-        },
-    });
-
-    console.log(res.data);
+    try {
+        const res = await axios.post(ankiUrl, {
+            action: "addNote",
+            version: 6,
+            params: {
+                note,
+            },
+        });
+        console.log(res);
+    } catch (err) {
+        console.log(err);
+    }
 }
-
-// if (!(await checkIfDeckExists("ankiEng"))) {
-//     await createAnkiDeck("ankiEng");
-// }
-
-// Send the request to import the card
-
-// axios
-//     .post(ankiUrl, card)
-//     .then((res) => {
-//         // console.log(res);
-//     })
-//     .catch((err) => {
-//         // console.log(err);
-//     });
