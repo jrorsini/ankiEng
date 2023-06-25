061aea3d701dbba1b2701499d48b496174d6fc1e
Jean-Roger Orsini <jeanroger.orsini@gmail.com>
2023-06-25 11:54:22 +0200

    changed the whole application's logic.

--- app.js
+++ app.js
@@ -1,53 +1,44 @@
-import { mainScrape } from "./utility/scrapeFuncs.js";
-import { logWordContent } from "./utility/log.js";
-import { exec } from "child_process";
 import {
-    askWhatWordToEnter,
-    whichExample,
-    whichDefinition,
-    whichTranslation,
-} from "./prompt.js";
+    getExamples,
+    getSynonyms,
+    getTranslations,
+    getDictionaryContent,
+} from "./utility/api.js";
+import { logWordContent } from "./utility/log.js";
+import { chooseExample, chooseTranslation, chooseSynonyms } from "./prompt.js";
 
 import axios from "axios";
 import { addCard } from "./anki.js";
+import cheerio from "cheerio";
 
 console.clear();
 
-const ankiEngNote = await mainScrape(
-    process.argv.slice(2).join(" ").toLowerCase().trim()
-);
+const usrInput = process.argv.slice(2).join(" ").toLowerCase().trim();
 
-logWordContent(ankiEngNote);
+console.log(`loading "${usrInput}"`);
 
-await whichTranslation.call(ankiEngNote);
+// const body = await axios.get(`https://www.dictionary.com/browse/${usrInput}`);
+// let $ = cheerio.load(body.data);
+// console.log($(`[data-type="pronunciation-text"]`).text());
+
+const ankiEngNote = { word: usrInput };
+
+await getDictionaryContent.call(ankiEngNote);
+await getSynonyms.call(ankiEngNote);
+await getTranslations.call(ankiEngNote);
+await getExamples.call(ankiEngNote);
+
+// console.log(ankiEngNote);
+
+// const ankiEngNote = await mainScrape(usrInput);
+
+logWordContent.call(ankiEngNote);
+
+ankiEngNote.translations && (await chooseTranslation.call(ankiEngNote));
+ankiEngNote.synonyms && (await chooseSynonyms.call(ankiEngNote));
+ankiEngNote.examples && (await chooseExample.call(ankiEngNote));
 
 console.log(ankiEngNote);
 
-/*
-
- logWordContent(
-        word,
-        ipa,
-        pronunciation,
-        definitions,
-        translations,
-        examples
-    );
-
-    // DEFINITION
-    let { typ, def } =
-        definitions.length > 1
-            ? await whichDefinition(definitions)
-            : definitions[0] || definitions;
-
-    // EXAMPLE
-    let example =
-        examples.length > 0 ? await whichExample(word, examples) : examples[0];
-
-    // TRANSLATION
-    let translation = await whichTranslation(translations);
-
-    // ADD ANKI CARD
-    await addCard(word, typ, pronunciation, ipa, def, example, translation);
-
-*/
+// ADD ANKI CARD
+await addCard.call(ankiEngNote);
