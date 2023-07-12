dc8b806b48b7f18665c9f400ea1fa43815b52657
Jean-Roger Orsini <jeanroger.orsini@gmail.com>
2023-07-12 12:36:59 +0200

    reversed to the original app.js code.

--- app.js
+++ app.js
@@ -21,61 +21,6 @@ console.log(`loading "${usrInput}"`);
 // let $ = cheerio.load(body.data);
 // console.log($(`[data-type="pronunciation-text"]`).text());
 
-const linguee_translations_res = await axios.get(
-    `https://linguee-api.fly.dev/api/v2/translations`,
-    {
-        params: { query: usrInput, src: "en", dst: "fr" },
-    }
-);
-
-const linguee_external_sources_res = await axios.get(
-    `https://linguee-api.fly.dev/api/v2/external_sources`,
-    {
-        params: { query: usrInput, src: "en", dst: "fr" },
-    }
-);
-
-const linguee_examples_res = await axios.get(
-    `https://linguee-api.fly.dev/api/v2/examples`,
-    {
-        params: { query: usrInput, src: "en", dst: "fr" },
-    }
-);
-
-const linguee_translations = linguee_translations_res.data.map((e) => ({
-    text: e.text,
-    pos: e.pos,
-    translations: e.translations
-        .filter((e) => e.featured)
-        .map((e) => ({
-            text: e.text,
-            pos: e.pos,
-            examples: e.examples,
-        })),
-}));
-
-const linguee_external_sources = linguee_external_sources_res.data
-    .map((e) => ({
-        src: e.src,
-        dst: e.dst,
-    }))
-    .sort((a, b) => a.src.length - b.src.length)
-    .slice(0, 8);
-
-const linguee_examples = linguee_examples_res.data.map((e) => ({
-    text: e.text,
-    translations: e.translations.map((e) => e.text).join(", "),
-}));
-
-console.clear();
-console.log(linguee_translations[0]);
-console.log(linguee_translations[0].translations[0]);
-console.log(linguee_translations[0].translations[0].examples);
-// console.log(linguee_examples);
-// console.log(linguee_external_sources);
-
-/*
-
 const ankiEngNote = { word: usrInput };
 
 await getDictionaryContent.call(ankiEngNote);
@@ -93,5 +38,3 @@ console.log(ankiEngNote);
 
 // ADD ANKI CARD
 await addCard.call(ankiEngNote);
-
-*/
