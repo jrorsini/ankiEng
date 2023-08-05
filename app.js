5ac27eb377c4b0b396ec2f665edb1dfbd4f54f9b
Jean-Roger Orsini <jeanroger.orsini@gmail.com>
2023-08-05 21:46:43 +0200

    changing app.js. Working on logging the linguee data.

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ import {
     getExamples,
     getTranslations,
     getDictionaryContent,
+    getLingueeData,
 } from "./utility/api.js";
 import { logWordContent } from "./utility/log.js";
 import { chooseExample, chooseTranslation } from "./prompt.js";
 
-import axios from "axios";
 import { addCard } from "./anki.js";
-import cheerio from "cheerio";
 
 console.clear();
 
@@ -16,23 +15,17 @@ const usrInput = process.argv.slice(2).join(" ").toLowerCase().trim();
 
 console.log(`loading "${usrInput}"`);
 
-// const body = await axios.get(`https://www.dictionary.com/browse/${usrInput}`);
-// let $ = cheerio.load(body.data);
-// console.log($(`[data-type="pronunciation-text"]`).text());
-
 const ankiEngNote = { word: usrInput };
 
-await getDictionaryContent.call(ankiEngNote);
-await getTranslations.call(ankiEngNote);
-// await getExamples.call(ankiEngNote);
+await getLingueeData.call(ankiEngNote);
 
-console.clear();
-logWordContent.call(ankiEngNote);
+// console.clear();
+// logWordContent.call(ankiEngNote);
 
-ankiEngNote.translations && (await chooseTranslation.call(ankiEngNote));
-ankiEngNote.examples && (await chooseExample.call(ankiEngNote));
+// ankiEngNote.translations && (await chooseTranslation.call(ankiEngNote));
+// ankiEngNote.examples && (await chooseExample.call(ankiEngNote));
 
 // console.log(ankiEngNote);
 
 // ADD ANKI CARD
-await addCard.call(ankiEngNote, "ankiEng", "ANKIENG_NOTE");
+// await addCard.call(ankiEngNote, "ankiEng", "ANKIENG_NOTE");
