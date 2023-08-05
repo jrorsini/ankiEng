5c90d862d0067af9e4fbfef4589349ccc57ba914
Jean-Roger Orsini <jeanroger.orsini@gmail.com>
2023-08-05 21:47:39 +0200

    learned on how to log table format.

--- linguee.js
+++ linguee.js
@@ -35,5 +35,13 @@ const linguee_translations = linguee_translations_res.data.map((e) => ({
 }));
 
 console.clear();
+const data = [
+    { id: 1, name: "John Doe", age: 30, email: "john@example.com" },
+    { id: 2, name: "Jane Smith", age: 28, email: "jane@example.com" },
+    { id: 3, name: "Bob Johnson", age: 35, email: "bob@example.com" },
+];
+
+console.table(data);
+
 console.log(translations);
 console.log(examples);
