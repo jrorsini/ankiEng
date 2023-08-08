436f53a3f2ad22fb8369f15c7d8681ea41859ce8
Jean-Roger Orsini <jeanroger.orsini@gmail.com>
2023-08-08 09:47:35 +0200

    changed reveal time for letters.

--- note/front.js
+++ note/front.js
@@ -170,7 +170,7 @@ setTimeout(() => {
                 wordElement.style.opacity = "1";
                 currentIndex++;
 
-                setTimeout(revealNextLetter, 50); // Adjust the delay as needed
+                setTimeout(revealNextLetter, 40); // Adjust the delay as needed
             }
         }
 
