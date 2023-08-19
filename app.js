02a487889a4f63d72476036671a129d8b3fad369
Jean-Roger Orsini <jeanroger.orsini@gmail.com>
2023-08-19 11:16:55 +0200

    removed some code logic and imported from other files.

--- app.js
+++ app.js
@@ -1,20 +1,44 @@
-import { getLingueeData } from "./utility/api.js";
-import { logLingueeData } from "./utility/log.js";
+import {
+    getLingueeData,
+    getReversoExamples,
+    getReversoTranslations,
+} from "./utility/api.js";
+import {
+    getClosestMatchingWord,
+    logLingueeData,
+    logReversoData,
+} from "./utility/log.js";
 import { chooseLingueeTranslation, chooseLingueeExample } from "./prompt.js";
+import Reverso from "reverso-api";
 
 import { addCard } from "./anki.js";
 
 // clear log.
 console.clear();
 
+// instanciate Reverso
+const reverso = new Reverso();
+
 // retrieve user input
+console.log(process.argv);
 const usrInput = process.argv.slice(2).join(" ").toLowerCase().trim();
+console.log(usrInput);
 
 // log user input loading
 console.log(`loading "${usrInput}"`);
 
 // create Anki note object.
 let ankiEngNote = { word: usrInput };
+
+let reverso_data = {};
+
+reverso_data = await getReversoTranslations.call(
+    reverso_data,
+    ankiEngNote.word
+);
+reverso_data = await getReversoExamples.call(reverso_data, ankiEngNote.word);
+
+reverso_data["word"] = usrInput;
 
 // get linguee's data
 ankiEngNote = await getLingueeData.call(ankiEngNote);
@@ -24,8 +48,13 @@ if (ankiEngNote !== undefined && ankiEngNote.translations.length > 0) {
     // clear log.
     console.clear();
 
-    // clear linguee's data.
+    // log reverso's data.
+    logReversoData(reverso_data);
+
+    // log linguee's data.
     logLingueeData(ankiEngNote);
+
+    console.log(`\n-----------------------\n`);
 
     // choose translation.
     ankiEngNote = await chooseLingueeTranslation.call(ankiEngNote);
@@ -41,4 +70,7 @@ if (ankiEngNote !== undefined && ankiEngNote.translations.length > 0) {
 } else {
     // clear log.
     console.clear();
+
+    // log reverso's data.
+    logReversoData(reverso_data);
 }
