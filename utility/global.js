34da98989eb61248d996fcf4707b9b3c5db5fe69
Jean-Roger Orsini <jeanroger.orsini@gmail.com>
2023-09-11 20:01:28 +0200

    added global.js

---
+++ utility/global.js
@@ -0,0 +1,8 @@
+// https://htmlcolorcodes.com/colors/shades-of-green/
+
+export const typeColor = {
+    noun: "#FA5F55",
+    verb: "#FFDB58",
+    adjective: "#90EE90",
+    adverb: "#CCCCFF",
+};
