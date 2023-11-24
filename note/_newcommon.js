48eec8f794b957536a4d35b0564d1d5ad9f44376
Jean-Roger Orsini <jean-rogerorsini@jean-rogers-macbook-pro.home>
2023-11-24 16:30:28 +0100

    unknown change.

--- note/_newcommon.js
+++ note/_newcommon.js
@@ -1,19 +1,15 @@
 function revealText(elementId) {
     const revealing_text = document.getElementById(elementId).innerHTML;
-    const revealing_text_match = revealing_text.match(/\|(.*?)\|/gi);
-
     document.getElementById(elementId).innerHTML = revealing_text.replace(
         /\|(.*?)\|/gi,
-        `<b>${revealing_text_match[0].slice(1, -1)}</b>`
+        (e) => `<b>${e.slice(1, -1)}</b>`
     );
 }
 
 function highlightToFill(elementId) {
     const revealing_text = document.getElementById(elementId).innerHTML;
-    const revealing_text_match = revealing_text.match(/\|(.*?)\|/gi);
-
     document.getElementById(elementId).innerHTML = revealing_text.replace(
         /\|(.*?)\|/gi,
-        `<b>${revealing_text_match[0].slice(1, -1).replace(/[\w]/g, '_')}</b>`
+        (e) => `<b>${e.slice(1, -1).replace(/[\w]/g, '_')}</b>`
     );
 }
