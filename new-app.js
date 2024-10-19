0d915ab0964865c1503899b045e820fc908e117c
Jean-Roger Orsini <jeanroger.orsini@gmail.com>
2024-10-19 18:33:56 +0200

    started refactoring this piece of shit.

---
+++ new-app.js
@@ -0,0 +1,48 @@
+// "astute" is a hard for me to guess. Maybe it'd be best if I had to go from french "perspicace"
+// "tétine" is a word that I should study both ways
+// "rougeurs", "driveway" needs a picture
+// "guetteur, homme de guet" is not a word easy to guess for lookout, should probably content with getting the english word, even from sentence example.
+// would be more convenient to have "remarquable" as a word to guess in english. More useful to use "standout" in english than guessing it.
+
+/**
+ * TO-DO-LIST
+ *
+ * Remove the definition field in Anki then also remove it from the app handlers.
+ * The dictionary.com resource should be used only for IPA.
+ * then change in the script to add a ruby row with the IPA in it.
+ * form now on, only guess from word or context.
+ */
+
+/**
+ * TO TEST
+ * wunderking
+ * oxpecker (undefined example)
+ */
+
+// API's handlers.
+import { getDictData, getWRefData } from './utils/new-api.js';
+import { logSearchResults } from './utils/searchResultslogs.js';
+// prompt questions.
+import {
+    chooseTranslation,
+    chooseDefinition,
+    chooseTranslationType,
+} from './prompt.js';
+
+import { startSpinner } from './utils/cli-loader.js';
+
+import { addCard } from './anki.js';
+
+// clear log.
+console.clear();
+
+// retrieve user input from terminal
+const usrInput = process.argv.slice(2).join(' ').toLowerCase().trim();
+
+// create Anki note object.
+let ankiEngNote = { word: usrInput };
+
+// DATA FETCH
+
+// get wordreference.com's & dictionary.com's data
+ankiEngNote = await getWRefData.call(ankiEngNote);
---
+++ utils/new-api.js
@@ -0,0 +1,74 @@
+// https://www.wordreference.com/english/abbreviationsWRD.aspx?dict=enfr&abbr=vi&src=WR
+
+import Reverso from 'reverso-api';
+import axios from 'axios';
+import { getClosestMatchingWord } from './searchResultslogs.js';
+import wr from 'wordreference-api';
+import { getTranslationsTypeList } from './translationTypes.js';
+
+const reverso = new Reverso();
+
+export async function getWRefData(word) {
+    try {
+        let wrData = await wr(word, 'en', 'fr');
+        const arr = wrData.translations.map((e) => e.translations);
+        let translations = [].concat(
+            ...arr.map((inArr) => [].concat(...inArr))
+        );
+        translations = translations.map((e) => ({
+            ...e,
+            from: e.from.trim().replaceAll('⇒', ''),
+            to: e.to.trim().replaceAll('⇒', ''),
+            example: {
+                from: e.example.from.length > 0 ? e.example.from[0] : '',
+                to: e.example.to.length > 0 ? e.example.to[0] : '',
+            },
+        }));
+
+        return {
+            ...this,
+            fromTypes: getTranslationsTypeList(translations),
+        };
+    } catch (err) {
+        return this;
+    }
+}
+
+export async function getIPA(word) {
+    try {
+        let dictionaryapiJSON = await axios.get(
+            `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+        );
+        return [
+            ...new Set(
+                dictionaryapiJSON.data.map((e) =>
+                    e.phonetic ? e.phonetic.replaceAll('/', '') : ''
+                )
+            ),
+        ][0];
+    } catch (error) {
+        console.error(error);
+        return;
+    }
+}
+
+export async function getDictData() {
+    try {
+        let dictionaryData = await axios.get(
+            `https://api.dictionaryapi.dev/api/v2/entries/en/${this.word}`
+        );
+
+        // assign IPA
+        this['ipa'] = [
+            ...new Set(
+                dictionaryData.data.map((e) =>
+                    e.phonetic ? e.phonetic.replaceAll('/', '') : ''
+                )
+            ),
+        ][0];
+
+        return this;
+    } catch (err) {
+        return this;
+    }
+}
