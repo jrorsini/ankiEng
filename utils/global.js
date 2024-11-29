b5ac09e8239577e8be540670d8c051646e11f610
Jean-Roger Orsini <jeanroger.orsini@gmail.com>
2024-11-29 11:31:02 +0100

    removed the color scheme global.js file.

--- utils/global.js
+++
@@ -1,8 +0,0 @@
-// https://htmlcolorcodes.com/colors/shades-of-green/
-
-export const typeColor = {
-    noun: "#FA5F55",
-    verb: "#FFDB58",
-    adjective: "#90EE90",
-    adverb: "#CCCCFF",
-};
